a30a57c708337b93332145eba057f9c0d9b34738
mike <mich.rivera84@gmail.com>
2023-03-12 14:40:00 -0400

    edits

--- 7-directory/pattern.js
+++ 7-directory/pattern.js
@@ -4,10 +4,11 @@ var hydra = new Hydra({
   });
 
 // s0.initScreen()
-// s0.initVideo() 
-s0.initImage("directory.png");
-// s3.initCam()
-// let cam = () => src(s3)
+s0.initImage("directory2.png") 
+let image = () => src(s0)
+// s0.initImage("directory.png");
+s3.initCam()
+let cam = () => src(s3)
 
 
 let red = (i) => src(i).color(1,0,0)
@@ -18,8 +19,13 @@ let sine1 = () => Math.abs(-0.01 * Math.sin(time * 0.5) + 0);
 let sine2 = () => Math.abs(0.1 * Math.sin(time * 0.5) + -1.57);
 let sine3 = () => Math.abs(1.0 * Math.sin(time * 1) + 1.5);
 let sine4 = () => 0.3 * Math.sin(time * 0.2) + 0.5;
-let sine5 = () => 0.0001 * Math.sin(time * 0.2) + sin5mod; // vert shift: .105
-  let sin5mod = () => 0.5 * Math.sin(time * 2.6) + 0.105; // vertshift: .105
+let sine5 = () => 0.0001 * Math.sin(time * 0.2) + sin5mod(); // vert shift: .105
+function sin5mod () {
+  let mod = 0.5 * Math.sin(time * 0.6) + 0.105
+  console.log(mod)
+  return mod // vertshift: .105
+} 
+console.log(sin5mod) 
 
 let random = () => Math.random();
 
@@ -40,7 +46,8 @@ let modGrid = () =>
   osc(gridSize, gridTime, 0).luma(0.04).rotate(sine1)
   .mult(osc(gridSize-150, gridTime, 0).luma(0.04).rotate(sine2).color(1, 0, -1))
     .modulate(osc(5, gridTime, 0))
-    .colorama(sine4)
+    .colorama(5)
+    .color(1.5, 0.2, sine1)
 
 grid()
   // .kaleid(100)
@@ -54,15 +61,15 @@ grid()
   .modulate(osc(5, gridTime, 0))
 // .mult(grid().modulate(osc(0.01, -0.5, 0)))
 .modulate(o0, sine5)
-.diff(cam(), 1)
-.modulate(cam(), sine5)
+.diff(image(), 1)
+.modulate(image(), sine5)
 // .modulate(modGrid(), sine5)
 // .layer(src(o1).color(sine1, 1, sine4), 1)
 // .kaleid([3, 10, 2, 5].fast(1))
 // .diff(src(o1)).sub(src(o1), 0.3)
 .out(o0);
 
-modGrid()
+image()
   // .kaleid([3, 10, 2, 5].fast(sine3))
 .out(o1);
 
